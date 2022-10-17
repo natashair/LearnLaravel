b2ea6e0db6e8b6620ebb45eb395ad0737c48ad6e
natashair <natasha.19040@mhs.unesa.ac.id>
2022-10-17 10:40:34 +0700

    Try Launc

---
+++ laravel/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome to ---
+
+This is a community for fellow humans and we love that you have joined us.
+
+All the best for you,<br>
+Wikwok
+@endcomponent
---
+++ laravel/resources/views/posts/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">    
+    <div class="row">
+        <div class="col-8">
+            <img src="/storage/{{ $post->image }}" class="w-100">
+        </div>
+        <div class="col-4">
+            <div>
+                <div class="d-flex align-items-center">
+                    <div class="pe-3">
+                        <img src="{{ $post->user->profile->profileImage() }}" class="rounded-cricle w-100" style="max-width: 40px;">
+                    </div>
+                    <div>
+                        <div class="font-weight-bold">
+                            <a href="/profile/{{ $post->user->id }}" style="text-decoration: none;">
+                                <span class="text-dark">
+                                    {{ $post->user->username }}
+                                </span>
+                            </a>
+                            <a href="#" class="ps-3" style="text-decoration: none;">Follow</a>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+
+                <p>
+                    <span class="font-weight-bold">
+                            <a href="/profile/{{ $post->user->id }}" style="text-decoration: none;">
+                                <span class="text-dark">
+                                    {{ $post->user->username }}
+                                </span>
+                            </a>
+                    </span>
+                    {{ $post->caption }}
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
